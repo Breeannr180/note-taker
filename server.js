40ebe6a642b0643ae6ee1eb838f4e36cc1270a8f
BreeAnn <breeannbond@gmail.com>
2023-05-18 18:16:48 -0700

    fixed server bug

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 //create server
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //set up middleware
 app.use(express.urlencoded({ extended: true }));
